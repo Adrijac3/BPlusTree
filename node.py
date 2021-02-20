11b34e72fce1ae4a03af32629e48dbdf3c231db0
Adrijac3 <30933610+Adrijac3@users.noreply.github.com>
2021-02-20 12:11:25 +0530

    removed comments and unnecessary code

--- node.py
+++ node.py
@@ -3,11 +3,10 @@ class nonLeafNode:
         self.keys = []
         self.children = []
         self.order = order
-        self.parent: nonLeafNode = None     #self referential pointer
+        self.parent: nonLeafNode = None
 
     def split(self):
         #create new left and right nodes around pivot
-        print("non leaf splitting")
         left = nonLeafNode(self.order)
         right = nonLeafNode(self.order)
         mid = self.order//2
@@ -23,15 +22,11 @@ class nonLeafNode:
         self.children.clear()
         self.children.append(left)
         self.children.append(right)
-        #TODO check if condition necessary or not
         #set correct parent for the children
         for c in left.children:
-            if isinstance(c,nonLeafNode):
-                c.parent = left
+            c.parent = left
         for c in right.children:
-            if isinstance(c,nonLeafNode):
-                c.parent = right
-
+            c.parent = right
         return self
 
 class leafNode(nonLeafNode):
@@ -39,14 +34,12 @@ class leafNode(nonLeafNode):
     def __init__(self,order):
         super().__init__(order)
         self.nextLeaf: LeafNode = None
-    #TODO remove garbage values of children from leaf nodes
     def split(self):
         #create a new intermediate node to hold pivot and merge it later to parent
         temp = nonLeafNode(self.order)
         #create new leaf node to put right half of pivot, store left half in self
         right = leafNode(self.order)
         mid = self.order//2
-
         #set up temp node
         temp.keys.append(self.keys[mid])
         temp.children.append(self)
@@ -54,12 +47,10 @@ class leafNode(nonLeafNode):
 
         #set appropriate values to the left and right nodes
         right.keys = self.keys[mid:]
-        # right.children = self.children[mid:]
         right.parent = temp
         right.nextLeaf = self.nextLeaf
 
         self.keys = self.keys[:mid]
-        # self.children= self.children[:mid]
         self.parent = temp
         self.nextLeaf = right
         return temp
@@ -71,17 +62,15 @@ class leafNode(nonLeafNode):
         else:
             i=0
             for k in self.keys:
-                if key == k:   #key already present. Add to count
+                if key == k:   #key already present. Added to count
                     return
                 elif key < k:  #insert key at appropriate position
                     self.keys = self.keys[:i] + [key] +self.keys[i:]
-                    # self.children = self.children[:i] + [child] + self.children[i:]
                     return
                 else:
                     i+=1
             #key is bigger than all other keys, so add at last
             self.keys.append(key)
-            # self.children.append(child)
 
     def findKey(self, key):
         i=0
