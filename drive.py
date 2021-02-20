5f881b3455c998cb15e69a6c46fe93d046b5c799
Adrijac3 <30933610+Adrijac3@users.noreply.github.com>
2021-02-20 12:12:36 +0530

    minor change

--- drive.py
+++ drive.py
@@ -6,19 +6,17 @@ def printError(msg):
     print()
 
 def handleQuery(bpt, query, param1, param2=None):
-    # print("parameters read from file: ",param1,param2)
     query = query.upper()
     if query == "INSERT":
         bpt.insert(param1)
     elif query == "FIND":
         bpt.search(param1)
     elif query == "COUNT":
-        bpt.count(param1)
+        bpt.countKey(param1)
     elif query == "RANGE":
         bpt.range(param1,param2)
     else:
         printError("Query not supported")
-        sys.exit()
 
 def getQueries(bpt, fileName):
     # try:
@@ -28,7 +26,6 @@ def getQueries(bpt, fileName):
                 if not l:
                     break
                 l=l.strip('\n').split(' ')
-                print(l)
                 if len(l) == 3:
                     query = l[0]
                     param1 = int(l[1])
@@ -40,17 +37,16 @@ def getQueries(bpt, fileName):
                     handleQuery(bpt,query, param1)
                 else:
                     printError("Wrong query format. Supported queries:")
-                    print("1. INSERT X \n 2. COUNT X. \n 3. FIND X \n 4. RANGE X Y \n")
-                    sys.exit()      
+                    print("1. INSERT X \n 2. COUNT X. \n 3. FIND X \n 4. RANGE X Y \n")     
     # except Exception as E:
     #     print(E)
-    #     sys.exit()
 
 def main():
     if len(sys.argv) != 2:
         printError("Error: syntax to run program: python3 drive.py <QueryFileName>")
     fileName = sys.argv[1]
     bpt = bPlusTree(3)
+    bpt.openFile()
     getQueries(bpt, fileName)
-
+    bpt.closeFile()
 main()
